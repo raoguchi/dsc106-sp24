0f14f376f6d083b5604c5e729e4448aa943481c3
Ryosuke Alex Oguchi <alexoguchi@gmail.com>
2024-04-16 16:05:05 -0700

    everything up to the second exercise

--- lab3/main.js
+++ lab3/main.js
@@ -1,1 +1,173 @@
 /* This file starts out empty; you will be adding to it in Lab 3 */
+console.log('Hello world');
+let greeting = 'Hello world';
+console.log(greeting);
+
+let testVar;
+console.log(testVar);
+console.log(typeof testVar);
+
+let testVar2 = null;
+console.log(testVar2);
+console.log(typeof testVar2);
+
+let studentAge = 25;
+studentAge = 26;
+
+const number1 = 2;
+const number2 = 3;
+const sum = number1 + number2;
+console.log(sum);
+
+let a = 1, b = 1, c = 2, d = 2;
+//Prefix increment
+console.log(++a);
+//Postfix increment
+console.log(b++);
+//Prefix decrement
+console.log(--c);
+//Postfix decrement
+console.log(d--);
+
+//Comparisons
+console.log("result of comparing with == ", "1" == 1);
+console.log("Result of comapring with === ", "1" === 1);
+
+//Functions
+function add(number1, number2) {
+    const sum = number1 + number2;
+    return sum;
+}
+
+const result = add(437, 34290093);
+console.log(result);
+
+//Objects and Methods
+const fullName = {
+    firstName: 'Ben',
+    lastName: 'Bitdiddle'
+}
+
+console.log(fullName.firstName);
+// or fullName['firstName']
+
+const personDetails = {
+    fullName: {
+        firstName: 'Ben',
+        lastName: 'Bitdiddle'
+    },
+    getName: function() {
+        return this.fullName.firstName + " " + this.fullName.lastName;
+    }
+}
+
+const personName = personDetails.getName();
+console.log(personName);
+
+//Arrays
+
+let arr = [];
+let fruits = ['Apple', 'Orange', 'Plum'];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+fruits[2] = 'Pear';
+fruits[3] = 'Lemon';
+
+console.log(fruits.length);
+
+let mix = ['hello', 123, personDetails];
+console.log(mix[2].fullName);
+
+
+//Data Structures
+console.log(fruits);
+const lemon = fruits.pop();
+console.log(fruits);
+
+fruits.push(lemon);
+console.log(fruits);
+
+//Shifting
+let fruitsBasket = ['Apple', 'Orange', 'Pear'];
+console.log(fruitsBasket.shift());
+console.log(fruitsBasket);
+
+//Unshifting
+let fruitsCollection = ['Apple'];
+fruitsCollection.push('Orange', 'Peach');
+fruitsCollection.unshift('Pineapple', 'Lemon');
+console.log(fruitsCollection)
+
+//Flow Control
+age = 21;
+if (age < 18) {
+    console.log("You're not an adult");
+} else {
+    console.log("You're an adult");
+}
+
+const experienceYears = 3;
+if (experienceYears < 5) {
+    console.log("You're a beginner");
+} else {
+    console.log("You're experienced");
+}
+
+const score = 54;
+if (score < 0) {
+    console.log("You provided an invalid score!");
+} else if (score < 50) {
+    console.log("You did not pass");
+} else {
+    console.log("You passed!")
+}
+
+const temperature = 72;
+if (temperature == null || temperature < -273.15) {
+    console.log("You provided an invalid temperature!");
+} else if (temperature < 0) {
+    console.log("It's freezing");
+} else {
+    console.log("It's above freezing");
+}
+
+//Loops
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+fruits.forEach(fruit => console.log(fruit, "with forEach"));
+
+//Exercise 1
+function areValidEmails(inputList) {
+    // your code here!
+    let result = true;
+    for (const email of inputList) {
+        if (email.endsWith('@ucsd.edu')) {
+            continue;
+        } else{
+            result = false;
+            break;
+        }
+    }
+    return result;
+  }
+  
+// Test cases:
+const list1 = ["alice@ucsd.edu", "bob@ucsd.edu", "eve@ucsd.edu"];
+console.log(areValidEmails(list1)); // should return true
+  
+const list2 = ["alice@ucsd.edu", "bob@ucsd.edu", "eve@harvard.edu"];
+console.log(areValidEmails(list2)); // should return false
+
+const list3 = ["alice", "bob@ucsd.edu"];
+console.log(areValidEmails(list3)); // should return false
+  
+
--- lab3/movies.js
+++ lab3/movies.js
@@ -1,1 +1,54 @@
 /* This file starts out empty; you will be adding to it in Lab 3 */
+async function populate() {
+    const requestURL = 'https://dsc106.com/resources/data/movies.json';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const movies = await response.json();
+
+    console.log("Numbers of movies in the Webfilms collection: ",
+Object.keys(movies).length);
+    let jimCarrey = movies.filter(movie =>
+        !movie.genres.includes("Comedy") &&
+        !movie.genres.includes("Family") &&
+        !movie.genres.includes('Animated') &&
+         movie.cast.includes("Jim Carrey"));
+    populateMovies(jimCarrey, "Jim Carrey Movies");
+    console.table(jimCarrey, 'title')
+}
+populate();
+
+
+function populateMovies(movies, categoryTitle) {
+    const section = document.querySelector('section');
+    const subsection = document.createElement('subsection');
+    section.appendChild(subsection);
+    const myH1 = document.createElement('h1');
+    myH1.textContent = categoryTitle;
+    subsection.appendChild(myH1);
+    for (const movie of movies) {
+        const myEntry = document.createElement('movie_entry');
+        const myH2 = document.createElement('h2');
+        const myPara2 = document.createElement('p');
+        const myPara3 = document.createElement('p');
+        const myPara4 = document.createElement('p');
+        const myList = document.createElement('ul');
+        myH2.textContent = movie.title;
+        myPara2.textContent = `Year: ${movie.year}`;
+        myPara3.textContent = 'Cast:';
+        const castList = movie.cast;
+        for (const actor of castList) {
+          const listItem = document.createElement('li');
+          listItem.textContent = actor;
+          myList.appendChild(listItem);
+        }       
+        if (movie.genres) {
+            myPara4.textContent = `Genres: ${movie.genres}`;
+        }
+        myEntry.appendChild(myH2);
+        myEntry.appendChild(myPara2);
+        myEntry.appendChild(myPara3);
+        myEntry.appendChild(myList);
+        myEntry.appendChild(myPara4);
+        subsection.appendChild(myEntry);
+      }
+}

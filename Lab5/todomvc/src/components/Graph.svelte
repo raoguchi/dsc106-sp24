22031412273362af885689d1618c533c2ff6f9b7
Ryosuke Alex Oguchi <alexoguchi@gmail.com>
2024-05-08 15:25:46 -0700

    Update Graph.svelte

--- Lab5/todomvc/src/components/Graph.svelte
+++ Lab5/todomvc/src/components/Graph.svelte
@@ -1,6 +1,22 @@
 <script>
+	import * as d3 from 'd3';
 	// our interactive data
 	export let todo_category = [];
+
+	let arcGenerator = d3.arc()
+			.innerRadius(10)
+			.outerRadius(100)
+			.padAngle(.02)
+			.cornerRadius(4);
+	let arc_data = [
+			{startAngle: 0, endAngle: 0.2},
+			{startAngle: 0.2, endAngle: 0.6},
+			{startAngle: 0.6, endAngle: 1.4},
+			{startAngle: 1.4, endAngle: 3},
+			{startAngle: 3, endAngle: 2* Math.PI}
+	];
+	
+
 
 	$: {
 		// interactive data here
@@ -10,6 +26,19 @@
 </script>
 
 <div class="visualization">
+	<svg width="500" height="500">
+		<g transform="translate(250,120)">
+				{#each arc_data as data, index}
+				<path 
+					d={arcGenerator({
+						startAngle: data.startAngle,
+						endAngle: data.endAngle
+					})}
+				/>
+				{/each}
+
+		</g>
+	</svg>
 </div>
 
 <style>
